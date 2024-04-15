f0824fb51d71a547ecf21662af02dcf79531adea
Poghossian <mariam.poghossian@etu.univ-amu.fr>
2024-04-15 08:57:57 +0200

    TP 1 Exercice 1

--- src/main/java/fr/amu/iut/exercice1/FenetreLogiciel.java
+++ src/main/java/fr/amu/iut/exercice1/FenetreLogiciel.java
@@ -1,6 +1,8 @@
 package fr.amu.iut.exercice1;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -31,91 +33,106 @@ public class FenetreLogiciel extends Application {
 
         BorderPane bp = new BorderPane();
 
-
+        MenuBar menuBar = new MenuBar();
 
         Menu fichier = new Menu("File");
         MenuItem fichierr= new MenuItem("New");
         MenuItem fichierrr = new MenuItem("Open");
         MenuItem fichierrrr = new MenuItem("Save");
-        MenuItem fichierrrrrr = new MenuItem("Close");
+        MenuItem fichierrrrrr = new MenuItem("Exit");
 
         fichier.getItems().addAll(fichierr, fichierrr, fichierrrr, fichierrrrrr);
+
 
 
         Menu edit = new Menu("Edit");
         MenuItem edit1= new MenuItem("Cut");
         MenuItem edit2 = new MenuItem("Copy");
         MenuItem edit3 = new MenuItem("Paste");
-
         edit.getItems().addAll(edit1 , edit2, edit3);
 
+        menuBar.getMenus().addAll(fichier, edit, new Menu("Help"));
 
         Menu help = new Menu("Help");
 
+        VBox leftBox = new VBox();
+        leftBox.setAlignment(Pos.CENTER);
+        leftBox.setSpacing(10);
 
-        MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().add(fichier);
-        menuBar.getMenus().add(edit);
-        menuBar.getMenus().add(help);
+
 
         bp.setTop(menuBar);
 
 
 
+        leftBox.setAlignment(Pos.CENTER);
+        leftBox.setSpacing(10);
+        Label buttonLabel = new Label("Boutons :");
+
+        Button button1 = new Button("Bouton 1");
+        Button button2 = new Button("Bouton 2");
+        Button button3 = new Button("Bouton 3");
+        leftBox.getChildren().addAll(buttonLabel,button1, button2, button3);
+
+        // add separator between left section and center section
+        bp.setLeft(new HBox(leftBox, new Separator(Orientation.VERTICAL)));
+
+
         // Création du conteneur principal
-        VBox vbox = new VBox();
-
-       // TableView<Object> tblCustomers = new TableView<>();
-        //tblCustomers.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-
-        //VBox.setVgrow( tblCustomers, Priority.ALWAYS );
-
-
-        Label l1 = new Label("Name: ");
-        Label l2 = new Label("Email: ");
-        Label l3 = new Label("Name: ");
-
-        Scene s1 = new Scene(l1 , 200 , 100);
+        VBox box = new VBox();
+        box.setAlignment(Pos.CENTER);
+        leftBox.setSpacing(10);
 
 
 
 
+        GridPane grilleFormulaire = new GridPane();
+        grilleFormulaire.setAlignment(Pos.CENTER);
+        grilleFormulaire.setHgap(10);
+        grilleFormulaire.setVgap(10);
+        grilleFormulaire.setPadding(new Insets(10));
 
 
+        grilleFormulaire.addRow(0, new Label("Name:"), new TextField());
+        grilleFormulaire.addRow(1, new Label("Email:"), new TextField());
+        grilleFormulaire.addRow(2, new Label("Password:"), new PasswordField());
 
 
+        HBox buttonBox = new HBox();
+        buttonBox.setAlignment(Pos.CENTER);
+        buttonBox.setSpacing(10);
+
+
+        buttonBox.getChildren().addAll(new Button("Submit"), new Button("Cancel"));
+
+        grilleFormulaire.add(buttonBox, 0, 3, 2, 1);
+
+        bp.setCenter(grilleFormulaire);
 
 
 
         // Création du bandeau en bas de la fenêtre
-        HBox bottomControls = new HBox();
-        bottomControls.setAlignment(Pos.BOTTOM_CENTER );
         Label a = new Label("Ceci est un label de bas de page");
-        bottomControls.getChildren().add( a );
+        VBox bas = new VBox(new Separator(Orientation.HORIZONTAL),a);
+        bas.setAlignment(Pos.CENTER);
+        bp.setBottom(bas);
 
-        bp.setBottom(bottomControls);
-
-
-
-
+        bp.setBottom(bas);
 
 
 
 
 
-        vbox.getChildren().add(menuBar);
-        vbox.getChildren().add(bottomControls);
-        vbox.getChildren().add(l1);
-        vbox.getChildren().add(l2);
-        vbox.getChildren().add(l3);
-     //   vbox.getChildren().add(tblCustomers);
+
+
+
 
 
 
 
 
         // Ajout du conteneur à la scene
-        Scene scene = new Scene(vbox);
+        Scene scene = new Scene(bp, 600, 400);
 
         // Ajout de la scene à la fenêtre et changement de ses paramètres (dimensions et titre)
         primaryStage.setScene( scene );
