f2e566a8b62090862af4ccd3318ba800906a0992
Poghossian <mariam.poghossian@etu.univ-amu.fr>
2024-04-15 09:34:53 +0200

    TP 2 Exercice 3

--- src/main/java/fr/amu/iut/exercice3/MaPremiereFenetreJavaFX.java
+++ src/main/java/fr/amu/iut/exercice3/MaPremiereFenetreJavaFX.java
@@ -1,6 +1,17 @@
 package fr.amu.iut.exercice3;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class MaPremiereFenetreJavaFX extends Application {
@@ -11,7 +22,50 @@ public class MaPremiereFenetreJavaFX extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("La page d'un Pro de JavaFX");
+
+        VBox vbox = new VBox();
+        vbox.setAlignment( Pos.CENTER );
+
+        Label helloLabel = new Label("Bonjour à tous !");
+        vbox.getChildren().add( helloLabel );
+
+        // Création de la scene
+        Scene scene = new Scene(vbox);
+
+
+        // Ajout d'un champ de saisi de texte de taille 180 pixels
+        TextField nameField = new TextField("Veuillez saisir un nom");
+        nameField.setMaxWidth(180.0d);
+        nameField.setFont( Font.font("Courier", FontWeight.NORMAL, 12) );
+        vbox.getChildren().add( nameField );
+
+        // Ajout d'un bouton avec du texte
+        Button button = new Button(/*"Dire bonjour"*/);
+        vbox.getChildren().add( button );
+
+
+
+
+        // Chargement de l'image
+        Image image = new Image( /*MaPremiereFenetreJavaFX.class.getResource(*/"exercice3/Bonjour.jpg"/*.)toString()*/ );
+
+        // Création d'un composant avec l'image peinte à l'intérieur
+        ImageView iv = new ImageView();
+        iv.setImage(image);
+
+        // Intégration de l'image dans le bouton
+        button.setGraphic( iv );
+
+        button.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            helloLabel.setText( "Bonjour à toi, "+nameField.getText() );
+        });
+
+
+        primaryStage.setScene( scene );
+
+        primaryStage.setTitle("Hello Application");
         primaryStage.show();
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(400);
     }
 }
