6fbbb850d3aa695479ffe120c33ad67961894295
Poghossian <mariam.poghossian@etu.univ-amu.fr>
2024-04-15 10:56:53 +0200

    TP 2 Exercice 4

--- src/main/java/fr/amu/iut/exercice4/Palette.java
+++ src/main/java/fr/amu/iut/exercice4/Palette.java
@@ -1,8 +1,11 @@
 package fr.amu.iut.exercice4;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -25,6 +28,55 @@ public class Palette extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        this.vert = new Button("vert");
+        this.rouge = new Button("rouge");
+        this.bleu = new Button("Bleu");
+
+        this.root = new BorderPane();
+        this.label = new Label();
+        this.panneau = new Pane();
+        this.bas = new HBox();
+
+        bas.setAlignment(Pos.BOTTOM_CENTER);
+
+        bas.getChildren().addAll(vert, rouge, bleu);
+        root.setBottom(bas);
+        root.setCenter(panneau);
+
+        vert.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent ->{
+            if (nbVert <2){
+                nbVert +=1;
+            }
+            else panneau.setStyle("-fx-background-color: rgb(49, 188, 164);");
+            label.setText("Vert choisi 3 fois");
+            BorderPane.setAlignment(label, Pos.CENTER);
+            root.setTop(label);
+        });
+
+        rouge.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent ->{
+            if (nbRouge <2){
+                nbRouge +=1;
+            }
+            else panneau.setStyle("-fx-background-color: rgb(200, 0, 22);");
+            label.setText("Rouge choisi 3 fois");
+            BorderPane.setAlignment(label, Pos.CENTER);
+            root.setTop(label);
+        });
+
+        bleu.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent ->{
+            if (nbBleu <2){
+                nbBleu +=1;
+            }
+            else panneau.setStyle("-fx-background-color: rgb(22, 0, 164);");
+            label.setText( "Bleu choisi 3 fois");
+            BorderPane.setAlignment(label, Pos.CENTER);
+            root.setTop(label);
+        });
+
+
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.setScene( scene );
         primaryStage.show();
     }
 }
