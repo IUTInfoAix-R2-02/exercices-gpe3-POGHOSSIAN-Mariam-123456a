cb728c658d6c8278cb14f689cbe33f6d2794cd22
Poghossian <mariam.poghossian@etu.univ-amu.fr>
2024-05-23 09:09:14 +0200

    TP 2 Exercice 6 (corrigé)

---
+++ src/main/java/tp1/exercice5/Obstacle.java
@@ -0,0 +1,13 @@
+package tp1.exercice5;
+
+
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+
+public class Obstacle extends Rectangle {
+
+    public Obstacle(double largeur, double hauteur, Paint paint) {
+        super(largeur, hauteur, paint);
+
+    }
+}
