2da2102721ca38907ff35606ef1fd8cc18c17dbf
Poghossian <mariam.poghossian@etu.univ-amu.fr>
2024-05-23 11:13:55 +0200

    TP 3 Exercice 8

--- src/main/java/fr/amu/iut/exercice8/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice8/LoginControl.java
@@ -1,12 +1,31 @@
 package fr.amu.iut.exercice8;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class LoginControl extends GridPane {
 
+    @FXML
+    private TextField userIdField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
     private void okClicked() {
+        String userId = userIdField.getText();
+        String password = passwordField.getText();
+        int passwordLength = password.length();
+
+        System.out.println("Identifiant Utilisateur: " + userId);
+        System.out.println("Mot de passe: " + "*".repeat(passwordLength));
     }
 
+    @FXML
     private void cancelClicked() {
+        userIdField.clear();
+        passwordField.clear();
     }
 }
